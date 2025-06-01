5b7e152958f7dfb14834c65b1c5ec0c1382ab87d
moonmd340 <pasidunuwan481@gmail.com>
2025-06-01 13:04:06 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -14,15 +14,18 @@ cmd({
     try {
         // Send image + caption
         await gojo.sendMessage(from, {
-            image: { url: "https://raw.githubusercontent.com/moonmd340/helper/refs/heads/main/IMG-20250601-WA0079.jpgqq" },
+            image: { url: "https://raw.githubusercontent.com/moonmd340/helper/refs/heads/main/IMG-20250601-WA0079.jpg" },
             caption: `⚡ MOON MAX is ALIVE ⚡\n\nSystem Status: ONLINE ✅\nBot Power Level: ∞\n\nCreated & Managed by: sayura\n\nType .menu to explore commands!`
         }, { quoted: mek });
 
         // Send voice message (PTT style)
-        
+        await gojo.sendMessage(from, {
             
+            mimetype: 'audio/mpeg',
+            ptt: true
+        }, { quoted: mek });
 
-     
+    } catch (e) {
         console.log(e);
         reply("Error in .alive command:\n" + e);
     }
